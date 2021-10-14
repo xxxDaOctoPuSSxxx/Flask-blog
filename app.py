b69a7a77c0b5bdd99773399d1219531e6b0b192d
Roman Hryshchenko <bookoowest@gmail.com>
2021-10-14 04:13:14 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return render_template("index.html")
+
+@app.route('/user/<name>')
+
+def user(name):
+    return "<h1>Hello {}</h1>".format(name)
+
+# Create CUstom Error Pages
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404 
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500

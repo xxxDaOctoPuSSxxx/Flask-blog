9b3dd6f3352b9e3f3ef094afb0ecdec5852e3643
Roman Hryshchenko <bookoowest@gmail.com>
2021-10-14 19:14:27 +0300

    Second Commit
    ADD index.html

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "SOME SECRET we must change them"
+
+# Create Form Class
+class NamerForm(FlaskForm):
+    name = StringField ("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 @app.route("/")
 
 def index():
     return render_template("index.html")
 
-@app.route('/user/<name>')
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfuly!")
 
-def user(name):
-    return "<h1>Hello {}</h1>".format(name)
+    return render_template("name.html",
+        name = name,
+        form = form)
+
 
 # Create CUstom Error Pages
 @app.errorhandler(404)

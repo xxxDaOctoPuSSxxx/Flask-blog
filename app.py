f9de5583f323fb953b22b20d9e847d50742cb358
Roman Hryshchenko <bookoowest@gmail.com>
2021-10-15 18:00:52 +0300

    Add DataBase SQLite

--- app.py
+++ app.py
@@ -1,18 +1,64 @@
+from enum import unique
 from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
+
+# Add DataBase
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+# Secret Key
 app.config['SECRET_KEY'] = "SOME SECRET we must change them"
+
+#Initialize Database
+db = SQLAlchemy(app)
+
+#Create Model Data Base
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Create A String
+def __repr__(self):
+    return '<Name %r>' % self.name
+
+class UserForm(FlaskForm):
+    name = StringField ("Enter Name", validators=[DataRequired()])
+    email = StringField("Enter @mail", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash("User Added Successfuly!")
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html", form=form, name=name, our_users=our_users)
 
 # Create Form Class
 class NamerForm(FlaskForm):
     name = StringField ("What's Your Name", validators=[DataRequired()])
     submit = SubmitField("Submit")
-@app.route("/")
 
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
@@ -32,7 +78,7 @@ def name():
         form = form)
 
 
-# Create CUstom Error Pages
+# Create Cstom Error Pages
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404 
